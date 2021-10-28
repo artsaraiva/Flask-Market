ff6e6bd23582fbd67c3a836f0968e37f55f877f9
Arthur <arthur.m.saraiva@hotmail.com>
2021-10-28 13:34:18 -0300

    Adicionadas validações nos formulários

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    name = StringField(label='Name:')
-    email = StringField(label='E-mail:')
-    password1 = PasswordField(label='Password')
-    password2 = PasswordField(label='Confirm Password')
+    name = StringField(label='Name:', validators=[Length(min=3, max=50), DataRequired()])
+    email = StringField(label='E-mail:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Submit')
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 
 @app.route("/")
@@ -17,7 +18,17 @@ def market_page():
 
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(name=form.username.data,
+                              email=form.email.data,
+                              password_hash=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}: #Se não existe erro de validação do formulário
+        for err_msg in form.errors.values():
+            print(f'Error while creating user: {err_msg}')
     return render_template('register.html', form=form)
