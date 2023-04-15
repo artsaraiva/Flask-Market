40a5786593c1fd846a12bcf0e7cba7d79d2a7ae9
Arthur <arthur.m.saraiva@hotmail.com>
2023-04-14 22:59:28 -0300

    Changes to the project to make it ready to deploy after cloning

--- market/__init__.py
+++ market/__init__.py
@@ -5,12 +5,12 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = 'c82da365023a9860a358c703'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "c82da365023a9860a358c703"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login_page'
-login_manager.login_message = 'Please sign in to access this page'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "login_page"
+login_manager.login_message = "Please sign in to access this page"
+login_manager.login_message_category = "info"
 from market import routes
